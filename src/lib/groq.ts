a9c942e9ddeeca1584012a5d38132109e1520212
DevMagic <devmagic313@gmail.com>
2026-02-19 02:29:44 +0500

    Add files via upload

---
+++ src/lib/groq.ts
@@ -0,0 +1,7 @@
+
+import Groq from "groq-sdk";
+
+export const groq = new Groq({
+    apiKey: import.meta.env.VITE_GROQ_API_KEY,
+    dangerouslyAllowBrowser: true, // Operating completely client-side
+});
